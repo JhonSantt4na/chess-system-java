c1f83ac2e53b97f0d46bfcf41736befd1352dc5c
Santt4naDev <jhonnsantt4na@gmail.com>
2025-01-01 20:33:59 -0300

    Starting to implement Board and Piece + Chess layer and printing the board

--- src/application/App.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import boardgame.Position;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Position pos = new Position(12, 20);
-    }
-}
